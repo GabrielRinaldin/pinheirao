cfb8be37531b838c4bc0b81e720ba4cc20cd6c29
GabrielRinaldin <gabrielrinaldin@hotmail.com>
2021-12-29 17:43:21 -0300

    Retirado img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,8 +82,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
-        <img src="https://cdn.shopify.com/s/files/1/0256/4738/2610/products/Bartx1000_1080x.png?v=1631713197%201080w"
-          alt="Pinheirão Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        {{-- <img src="#"
+          alt="Pinheirão Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
         <span class="brand-text font-weight-light">Conjunto Pinheirão</span>
       </a>
 
@@ -92,9 +92,9 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img
-              src="https://cdn.shopify.com/s/files/1/0256/4738/2610/products/Bartx1000_1080x.png?v=1631713197%201080w"
-              class="img-circle elevation-2" alt="User Image">
+            {{-- <img
+              src="#"
+              class="img-circle elevation-2" alt="User Image"> --}}
           </div>
           <div class="info">
             <a href="#" class="d-block">@guest Bem Vindo! <br> Logue para ter acesso
