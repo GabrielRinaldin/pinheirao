365e7990f2155c524be0a7669961782ccf8158b6
GabrielRinaldin <gabrielrinaldin@hotmail.com>
2022-01-09 18:33:35 -0300

    fix

--- resources/views/suggestion/index.blade.php
+++ resources/views/suggestion/index.blade.php
@@ -5,7 +5,6 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Movimentações!</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
