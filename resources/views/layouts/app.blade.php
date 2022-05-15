5408a9df2bed5b3635b9732243fabcb87c2e1d9b
GabrielRinaldin <gabrielrinaldin@hotmail.com>
2022-05-15 19:04:49 -0300

    teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>Laravel</title>
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
@@ -26,6 +25,13 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+
   <!--Admin.IO-->
   <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
@@ -34,149 +40,27 @@
 
 <body class="sidebar-mini layout-footer-fixed" style="height: auto;">
 
-  <!-- Site wrapper -->
   <div class="wrapper">
 
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="bi bi-justify"></i> </a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="{{url('/home')}}" class="nav-link">Ínicio</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="{{url('/home')}}" class="nav-link">Contato</a>
-        </li>
-      </ul>
-
-      <ul class="navbar-nav ms-auto">
-        <!-- Authentication Links -->
-        @guest
-        @if (Route::has('login'))
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-        </li>
-        @endif
-
-        @else
-        <li class="nav-item dropdown">
-          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }}
-          </a>
-
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-              {{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-              @csrf
-            </form>
-          </div>
-        </li>
-        @endguest
-      </ul>
-
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
       <a href="#" class="brand-link">
-        {{-- <img src="#" alt="Pinheirão Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-        <span class="brand-text font-weight-light">Conjunto Pinheirão</span>
+        <span class="brand-text font-weight-light">JR Consultoria</span>
       </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            {{-- <img src="#" class="img-circle elevation-2" alt="User Image"> --}}
-          </div>
-          <div class="info">
-            <a href="#" class="d-block">@guest Bem Vindo! <br> Logue para ter acesso
-              @else{{Auth::user()->name}}@endguest</a>
-          </div>
-        </div>
-
-        @auth
-        <!--Libera somente logado-->
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-                   with font-awesome or any other icon font library -->
-            <li class="nav-item">
-              <a href="{{url('/home')}}" class="nav-link">
-                <i class="bi bi-house"></i>
-                <p>
-                  Ínicio
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="{{url('/suggestion')}}" class="nav-link">
-                <i class="bi bi-card-checklist"></i>
-                <p>
-                  Sugestões
-                </p>
-              </a>
-            </li>
-            @if(Auth::user()->user_type == 'sindico')
-            <li class="nav-item">
-              <a href="{{url('/user')}}" class="nav-link">
-                <i class="bi bi-people"></i>
-                <p>
-                  Moradores
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="{{url('/movement')}}" class="nav-link">
-                <i class="bi bi-cash-coin"></i>
-                <p>
-                  Movimentações
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="{{url('/followup/'.Auth::user()->id)}}" class="nav-link">
-                <i class="bi bi-wallet"></i>
-                <p>
-                  Minhas Movimentações
-                </p>
-              </a>
-            </li>
-            @endif
-          </ul>
-        </nav>
-        @endauth
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
     </aside>
 
-
-    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper" style="min-height: 100vh;">
       <main class="py-4">
         @yield('content')
       </main>
     </div>
-    <!-- /.content-wrapper -->
 
     <footer class="main-footer">
       <div class="float-right d-none d-sm-block">
         <b>Versão</b> 1.0
       </div>
-      <strong>Copyright © 2021 <a href="#">Condomínio Pinheirão</a>.</strong> Todos os direitos reservados.
+      <strong>Copyright © 2022 <a href="#">PJM</a>.</strong> Todos os direitos reservados.
+      <small>Desenvolvido por <a href="https://www.linkedin.com/in/gabriel-machado-rinaldin-b1a9261ab/">Gabriel M.
+          Rinaldin</a>.</small>
     </footer>
     <!-- /.control-sidebar -->
   </div>
@@ -184,7 +68,7 @@
 
   <!-- jQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
 
 </body>
 
