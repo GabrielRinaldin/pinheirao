1ed79c5ffa32b89c82d87f8b678e0057ba1d4529
GabrielRinaldin <gabrielrinaldin@hotmail.com>
2022-05-15 19:02:02 -0300

    return

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('user_type', ['sindico', 'colaborador', 'morador' ])->default('morador');
+            $table->integer('house_number')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- resources/lang/pt-BR/validation.php
+++ resources/lang/pt-BR/validation.php
@@ -178,12 +178,5 @@ return [
         'remember'              => 'lembrar-se de mim',
         'user_type'              => 'Tipo de usuário',
         'house_number'              => 'Número da casa',
-        'contrato_social' => 'Contrato Social Consolidado* (atualizado, em doc, docx ou pdf):',
-        'rg' => 'RG do Responsável:',
-        'cpf' => 'CPF do Responsável:',
-        'cnh' => 'CNH do Responsável:',
-        'comprovante_residencia' => 'Comprovante de Residência:',
-        'pgdas' => 'PGDAS',
-        'file' => 'Arquivos',
     ],
 ];
